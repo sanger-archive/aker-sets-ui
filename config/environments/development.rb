f30c72fdf829ca971fe285601bad7195b2218da4
Chris Smith <cs24@sanger.ac.uk>
2016-10-06 14:24:26 +0100

    Fixed issue with request headers
    Allowed local precompiling

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,10 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Use a different URL for serving assets in development mode, and pass all requests to Sprockets
+  # http://guides.rubyonrails.org/asset_pipeline.html#local-precompilation
+  config.assets.prefix = "/dev-assets"
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
